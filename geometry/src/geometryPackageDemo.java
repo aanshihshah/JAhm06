0632aceb69cfb23b325bb103d6a28ae61528fe34
aanshihshah <169074087+aanshihshah@users.noreply.github.com>
2024-05-23 20:38:46 +0530

    Add files via upload

---
+++ geometry/src/geometryPackageDemo.java
@@ -0,0 +1,17 @@
+import geometry.Circle;
+import geometry.Rectangle;
+import geometry.Triangle;
+
+public class geometryPackageDemo {
+	public static void main(String[] args) {
+		
+			Circle c=new Circle(5);
+			 System.out.println(c);
+			 Rectangle r=new Rectangle(5,6);
+			 System.out.println(r);
+			 Triangle t=new Triangle(5,6);
+			 System.out.println(t);
+		
+
+    }
+}
