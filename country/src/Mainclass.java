cb4d0e0995480fc644feca79f0f160d38233e7de
aanshihshah <169074087+aanshihshah@users.noreply.github.com>
2024-05-24 13:49:01 +0530

    Add files via upload

--- country/src/Mainclass.java
+++ country/src/Mainclass.java
@@ -8,8 +8,9 @@ public class Mainclass {
 		
 		Country c1=new Country("india");
 		State s2 =new State("gujrat");
-		Person p =new Person(c1,s2,"Aanshi");
+		Person p =new Person("Aanshi",c1,s2,500000,21);
 		System.out.println(p);
+		TexClass t= new TexClass(p);
 
 
 	}
--- country/src/Person.java
+++ country/src/Person.java
@@ -1,17 +1,53 @@
 import country.*;
 import State.*;
 public class Person {
+	String name;
 	Country c1;
-	State s1;
-	String pname;
-	public Person(Country c1, State s1, String pname) {
+	State s2;
+	int salary,age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public  Person(String name, Country c1, State s2, int salary, int age) {
+		this.name = name;
 		this.c1 = c1;
-		this.s1 = s1;
-		this.pname = pname;
+		this.s2 = s2;
+		this.salary = salary;
+		this.age = age;
 	}
 	@Override
 	public String toString() {
-		return "Person [c1=" + c1 + ", s1=" + s1 + ", pname=" + pname + "]";
+		return "person [name=" + name + ", c1=" + c1 + ", s2=" + s2 + ", salary=" + salary + ", age=" + age + "]";
 	}
+	
+//	public person(String name, Country c1, State s2) {
+//		
+//		this.name = name;
+//		this.c1 = c1;
+//		this.s2 = s2;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "person [name=" + name + ", c1=" + c1 + ", s2=" + s2 + "]";
+//	}
+//	
+	
+
 	
 }
