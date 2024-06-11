89cf754f487ea253ef7209724cc0d864d4fbdeb5
Aanshi Shah <169074087+aanshihshah@users.noreply.github.com>
2024-06-11 10:35:25 +0530

    Add files via upload

---
+++ area/src/area/MainClass.java
@@ -0,0 +1,12 @@
+package area;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Circle c=new Circle();
+		c.CircleArea(6);
+
+	}
+
+}
